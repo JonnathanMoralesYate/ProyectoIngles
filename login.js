d5a72bf4dc4253a800314e46240ea7544bc787e4
CAJUGATO76 <cajugato76@hotmail.com>
2024-09-08 03:41:36 -0500

    actualización de 8 de septiembre

--- login.js
+++ login.js
@@ -12,43 +12,79 @@ document.getElementById("login_form").addEventListener("submit", function(event)
     })
     .then(response => response.json())
     .then(data => {
-        let formularioInicio=document.getElementById('login_form');
+               
         const messageElement = document.getElementById("message");
         let casillas= document.querySelectorAll('.input_sign');
-        let welcom= document.getElementById('Bienvenida');
-        let coment= document.getElementById('comentarios');
-        let log_out=document.getElementById('log_out');
-        let login= document.getElementById('login_inic');
-        let register= document.getElementById('registro');
-
         if (data.status === "success") {
-
-            login.style.display="none";
-            register.style.display="none";
-            coment.style.display="block";
-            log_out.style.display= "block";
-            welcom.innerHTML = `Hello <span style="color: #ff9b62;">${data.nombre}</span>. Welcome to How much do you know about PANELA!!`;
-            welcom.style.fontWeight="bold";
-            welcom.style.color="#7dcea0";
-            welcom.style.backgroundColor="white";
-            welcom.style.borderRadius="4px";
-           formularioInicio.style.display="none";
+           
+            const nombreUsuario=data.nombre;
+            localStorage.setItem('sesionIniciada','true');
+            localStorage.setItem('elUser', nombreUsuario );          
         } else {
-            messageElement.style.color = "red";
+          
             messageElement.textContent = data.message;
-            messageElement.style.backgroundColor="#d6eacc";
-            messageElement.style.fontWeight="bold";
-            messageElement.style.fontStyle="italic";
+            inicioFailed(messageElement);           
         }
         for(let i=0; i< casillas.length; i++){
             casillas[i].value="";
         }
+        location.reload();
     })
+    
     .catch(error => console.error('Error:', error));
+    
+   
 });
+configInicio();
+
+//función para inicio de sesion exitosa:
+function inicioSucces(){
+    let formularioInicio=document.getElementById('login_form');
+    formularioInicio.style.display="none";
+
+}
+//función inicio fallido:
+function inicioFailed(elemento){
+    elemento.style.color = "red";
+    elemento.style.backgroundColor="#d6eacc";
+    elemento.style.fontWeight="bold";
+    elemento.style.fontStyle="italic";
+}
+//función que configura la sesión iniciada:
+function configInicio(){     
+    
+    let welcom= document.getElementById('Bienvenida');
+    let coment= document.getElementById('comentarios');
+    let log_out=document.getElementById('log_out');
+    let login= document.getElementById('login_inic');
+    let register= document.getElementById('registro');
+    const sesionIniciada= localStorage.getItem('sesionIniciada');
+    const nombreU= localStorage.getItem('elUser');    
+    
+
+    if(sesionIniciada==='true'){
+        login.style.display="none";
+        register.style.display="none";
+        coment.style.display="block";
+        log_out.style.display= "block";
+
+        welcom.innerHTML = `Hello <span style="color: red;">${nombreU}</span>, Welcome to how much do you know about PANELA!!`;
+        
+        welcom.style.fontWeight="bold";
+        welcom.style.color="#7dcea0";
+        welcom.style.backgroundColor="white";
+        welcom.style.borderRadius="4px";     
+    }else{
+        login.style.display="block";
+        register.style.display="block";
+        coment.style.display="none";
+        log_out.style.display= "none";
+        welcom.style.display="none";
+    }   
+    
+}
 
 //Insertando comentario de registro exitoso o usuario ya ha sido registrado.
-
 document.getElementById("sign_up").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
 
@@ -76,3 +112,35 @@ document.getElementById("sign_up").addEventListener("submit", function(event) {
     })
     .catch(error => console.error('Error:', error));
 });
+
+
+//Función para cerrar sesión:
+function cerrarSesion() {
+    fetch('logout.php')
+    .then(response => response.text())
+    .then(result => {
+        if (result === 'success') {
+            localStorage.removeItem('sesionIniciada');
+            configInicio();
+        }
+    });
+}
+
+
+
+/*Métodos de 'localStorage':
+    1.- setItem(key,value): Almacena un valor bajo una clave.
+        Ej: localStorage.setItem(sesionIniciada, true);
+        Ej: localStotage.setItem('nombre', 'Carlos');
+    2.- getItem(key) : Recupera el valor almacenado bajo una clave.
+            Ej: const nombre= localStorage.getItem('sesionIniciada'); //true
+    3.- removeItem(key) : elimina el ítem bajo una clave.
+            Ej: localStorage.removeItem('nombre');
+    4.- key(index): recupera la clave del ítem en una posición específica.
+
+    5.- length: propiedad que devuelve el número total de ítems almacenados.
+            Ej: console.log(localStorage.length); // 1
+    6.- clear(): elimina todos los ítems del almacenamiento local.
+            Ej: localStorage.clear();
+    
+*/
